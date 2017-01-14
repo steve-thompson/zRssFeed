60cccde7c6f0507597a497b1fd883f91baa9d8c4
Steve Thompson <stevethompson@warwickshire.gov.uk>
2017-01-14 11:00:46 +0000

    Updates

--- wcc_files/homepage-slider-test.js
+++ wcc_files/homepage-slider-test.js
@@ -1,16 +1,14 @@
 function GetContent(feedUrl) {
-	var feedApiGetJSON = '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=5&callback=?&q=';
-	jQuery.ajax({
-		url: feedApiGetJSON + feedUrl,
-		dataType: 'jsonp',
-		jsonpCallback: 'JsonpCallback'
-	});
-}
+	//var feedApiGetJSON = '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=5&callback=?&q=';
+	var api = "https://query.yahooapis.com/v1/public/yql?q=" + encodeURIComponent("select * from xml where url = '" + feedUrl + "'&format=json&callback=cbFunc");
 
-function JsonpCallback(data) {
-	if (data.responseStatus == '200') {
-		// alert(data.responseData.feed.title);
-		// above just for debugging
+  //var yql = 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="' + feedUrl + '"') + '&format=json&callback=cbFunc';
+  $.getJSON( api, cbFunc );
+}
+   
+function cbFunc(data) {
+  if ( data.results[0] ) {
+
 		for (var i = 0; i < data.responseData.feed.entries.length; i++) {
 			sampleMedia = data.responseData.feed.entries[i].mediaGroups;
 			subSampleMedia = sampleMedia[0].contents[0].thumbnails[0];
@@ -30,3 +28,43 @@ function JsonpCallback(data) {
 		jQuery('#latest-news').append('<div class="item slide">Sorry, something went wrong when we tried to get those stories for you. We had this error: ' + data.responseDetails + '</div>');
 	}
 }
+
+	// $.ajax({
+	// 	url: "http://query.yahooapis.com/v1/public/yql",
+
+	// 	// The name of the callback parameter, as specified by the YQL service
+	// 	jsonp: "callback",
+
+	// 	// Tell jQuery we're expecting JSONP
+	// 	dataType: "jsonp",
+
+	// 	// Tell YQL what we want and that we want JSON
+	// 	data: {
+	// 	  q: "select * from xml where url = '" + feedUrl + "'",
+	// 	  format: "json"
+	// 	},
+
+	// 	// Work with the response
+	// 	success: function( response ) {
+	// 	  console.log( response ); // server response
+	// 	}
+	// });
+
+
+	// jQuery.ajax({
+	// 	url: feedApiGetJSON,
+	// 	dataType: "jsonp",
+ //    jsonp: "callback",
+ //    jsonpCallback: "quote"
+	// });
+
+	// $.getJSON(api, function(data){
+	// 	if (data.query.count > 0) {
+	
+	// 		console.log(data);
+	// 		// Process the feeds
+	// 		//_process(e, data.query.results, options);
+
+			
+	// 	}
+	// });
